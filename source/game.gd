4f813f85ce9dce2c9acde325473edf31b7d60626
Aiden Storey <git@storey.dev>
2023-03-30 21:44:06 -0400

    Frame break

--- source/game.gd
+++ source/game.gd
@@ -1,8 +1,16 @@
-extends Node2D
+extends Control
 
+
+onready var cursor: ColorRect = $cursor
 
 func _ready() -> void:
 	JavaScript.eval("console.log('this works as expected')")
 	JavaScript.eval("const m = document.getElementById('main'); console.log(m.clientWidth, m.clientHeight);")
 	var a = JavaScript.eval("document.getElementById('main').clientWidth")
 	print("From inside Godot: ", a)
+
+
+func _process(delta: float) -> void:
+	var position: Vector2 = get_global_mouse_position()
+
+	cursor.rect_position = position - Vector2(10.0, 10.0)
