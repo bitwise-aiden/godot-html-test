2bef0677342018e0cd14cfc96eaae947dfbdf268
Aiden Storey <git@storey.dev>
2023-03-30 21:44:06 -0400

    Transparent approach

--- source/game.gd
+++ source/game.gd
@@ -4,13 +4,13 @@ extends Control
 onready var cursor: ColorRect = $cursor
 
 func _ready() -> void:
-	JavaScript.eval("console.log('this works as expected')")
-	JavaScript.eval("const m = document.getElementById('main'); console.log(m.clientWidth, m.clientHeight);")
-	var a = JavaScript.eval("document.getElementById('main').clientWidth")
-	print("From inside Godot: ", a)
+	get_tree().get_root().set_transparent_background(true)
 
 
 func _process(delta: float) -> void:
 	var position: Vector2 = get_global_mouse_position()
+
+	position.x = min(1270.0, max(10.0, position.x))
+	position.y = min( 710.0, max(10.0, position.y))
 
 	cursor.rect_position = position - Vector2(10.0, 10.0)
